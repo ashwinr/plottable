1176e20a55dff783c058b0899f34317e43fb8c79
Daniel Mane <danmane@gmail.com>
2014-02-26 10:49:42 -0800

    Refactor truncateTextToLength out of Label and into Utils. Add better unit testing. Change API so it no longer modifies the element as a side-effect, but instead takes the text, element, and length, measures the text in the element, returns shorter text, and returns the textElement to its original state.
    I think this is a better API so that it can be called multiple times without
    getting into weird states, and its easier to reason about functions without
    side effects.

--- examples/tscDemo.ts
+++ examples/tscDemo.ts
@@ -1,23 +1,12 @@
 ///<reference path="exampleReference.ts" />
 
 module TSCDemo {
-
-  var yScale = new LinearScale();
-  var xScale = new LinearScale();
-  var left = new YAxis(yScale, "left");
-  var data = makeRandomData(1000, 200);
-  var lineRenderer = new LineRenderer(data, xScale, yScale);
-  var bottomAxis = new XAxis(xScale, "bottom");
-
-  var chart = new Table([[left, lineRenderer]
-                        ,[null, bottomAxis]]);
-
-  var outerTable = new Table([ [new TitleLabel("A Chart")],
-                               [chart] ])
-
   var svg = d3.select("#table");
-  outerTable.anchor(svg);
-  outerTable.computeLayout();
-  outerTable.render();
+  var color = new ColorScale("Category10");
+  color.domain(["foo", "bar", "baz"]);
+  var legend = new Legend(color);
+  legend.anchor(svg).computeLayout().render();
 
+  color.domain(["loooooooooooooooong string", "bazbarbaz", "fizzbuzzbam"]);
+  legend.render();
 }
